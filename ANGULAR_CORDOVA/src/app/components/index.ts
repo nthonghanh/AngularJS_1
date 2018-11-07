b3691b5e79b2b288a683224048e24c293d073dc0
Nguyen Thi Hong Hanh <nthonghanh@tma.com.vn>
2018-11-07 17:58:22 +0700

    Custom Cordova for Android platform.

---
+++ ANGULAR_CORDOVA/src/app/components/index.ts
@@ -0,0 +1,4 @@
+export { HeroesComponent } from './heroes/heroes.component';
+export { HeroDetailComponent } from './hero-detail/hero-detail.component';
+export { MessagesComponent } from './messages/messages.component';
+export { DashboardComponent } from './dashboard/dashboard.component';
